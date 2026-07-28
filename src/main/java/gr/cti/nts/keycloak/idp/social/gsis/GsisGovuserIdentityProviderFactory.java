124572dc909a8d10ad7ee603c5a05dc8da9b525c
XhmikosR <xhmikosr@gmail.com>
2026-07-28 11:31:15 +0300

    Update URLs

--- src/main/java/gr/cti/nts/keycloak/idp/social/gsis/GsisGovuserIdentityProviderFactory.java
+++ src/main/java/gr/cti/nts/keycloak/idp/social/gsis/GsisGovuserIdentityProviderFactory.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2021 Greek School Network and Networking Technologies Directorate (http://nts.cti.gr/),
+ * Copyright 2021 Greek School Network and Networking Technologies Directorate (https://www.cti.gr/nts-department/),
  * Konstantinos Togias (ktogias@cti.gr) and/or their affiliates and other contributors as indicated
  * by the @author tags.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  * in compliance with the License. You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
